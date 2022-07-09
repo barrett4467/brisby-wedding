9dec08f968db35d7e3870cc6b0a0a4dfc7112769
Rebecca Barrett <barrett4467@gmail.com>
2022-07-09 14:43:54 -0600

    fleshed out rsvp form

--- src/components/Rsvp.js
+++ src/components/Rsvp.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
 import Popup from "./Popup";
 import { useState } from "react";
+import RSVPform from "../elements/Rsvpform";
 
 function RSVP () {
     const [buttonPopup, setButtonPopup] = useState(false);
@@ -14,9 +15,10 @@ function RSVP () {
             <h3>Golden, Colorado</h3>
             <h3>October 15th, 2022</h3>
             <p>*insert countdown timer here*</p>
+            <p>Please respond by September 17th</p>
             <button onClick={() => setButtonPopup(true)}>Respond here</button>
             <Popup trigger={buttonPopup} setTrigger={setButtonPopup} >
-                <h3>Popup</h3>
+                <RSVPform />
             </Popup>
 
             <Link to="/details">view details</Link>
--- src/elements/Rsvpform.js
+++ src/elements/Rsvpform.js
@@ -1,25 +1,75 @@
 import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
-function ContactForm() {
+function RSVPForm() {
   const [state, handleSubmit] = useForm("meqnblnr");
   if (state.succeeded) {
-      return <p>Thanks for joining!</p>;
+      return <p>Thank you for your RSVP! We can't wait to celebrate with you!</p>;
   }
   return (
       <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email Address
-      </label>
-      <input
-        id="email"
-        type="email" 
-        name="email"
-      />
+        <label htmlFor="email">
+            Email Address
+        </label>
+        <input
+            id="email"
+            type="email" 
+            name="email"
+        />
       <ValidationError 
         prefix="Email" 
         field="email"
         errors={state.errors}
       />
+        <label htmlFor="first-name">
+           First Name
+        </label>
+        <input
+            id="first-name"
+            type="text" 
+            name="first-name"
+        />
+        <label htmlFor="last-name">
+           Last Name
+        </label>
+        <input
+            id="last-name"
+            type="text" 
+            name="last-name"
+        />
+      <ValidationError 
+        prefix="Name" 
+        field="name"
+        errors={state.errors}
+      />
+        <label for="attending">Will you be Attending?</label>
+        <select name="attending" id="attending" required="">
+            <option value="" selected="" disabled="">Select</option>
+            <option value="Yes">Yes</option>
+            <option value="No">No</option>
+        </select>
+        <label for="plus">And with how many guests?</label>
+        <select name="plus" id="plus" required="">
+            <option value="0" selected="">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+        </select>
+        <label htmlFor="plus-names">
+          Please let us know who!
+        </label>
+        <input
+            id="plus-names"
+            type="text" 
+            name="plus-names"
+        />
+        <ValidationError 
+            prefix="Plus-names" 
+            field="plus-names"
+            errors={state.errors}
+        />
+        <label htmlFor='message'>
+            Any dietary restrictions or things we should know?
+        </label>
       <textarea
         id="message"
         name="message"
@@ -35,9 +85,4 @@ function ContactForm() {
     </form>
   );
 }
-function App() {
-  return (
-    <ContactForm />
-  );
-}
-export default App;
+export default RSVPForm;
