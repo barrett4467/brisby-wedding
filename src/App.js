08ae69dca44f4af79e4fc37868ace39a5d2f55b3
Rebecca Barrett <barrett4467@gmail.com>
2022-07-17 21:16:22 -0600

    Added background image to home page

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import Photos from "./components/Photos";
 import Registry from "./components/Registry";
 import RSVP from "./components/Rsvp";
 import Details from "./components/Details";
-
+import styled from "styled-components";
+import img from "./images/engagement3.jpg";
 
 function App() {
   return (
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<About />} />
-        <Route path="/photos" element={<Photos />} />
-        <Route path="/details" element={<Details />} />
-        <Route path="/registry" element={<Registry />} />
-        <Route path="/rsvp" element={<RSVP />} />
-    
-      </Routes>
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/photos" element={<Photos />} />
+          <Route path="/details" element={<Details />} />
+          <Route path="/registry" element={<Registry />} />
+          <Route path="/rsvp" element={<RSVP />} />
+      
+        </Routes>
       </BrowserRouter>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,26 @@
 import React from "react";
 import Navbar from "./Navbar";
 import styled from "styled-components";
+import img from "../images/engagement3.jpg";
 
-const Main = styled.button`
-    background: red;
+const Main = styled.div`
+    background-image: url(${img});
+    width: 100vw;
+    height: 100vh;
+    background-size: cover;
+    background-attachment: fixed;
+    background-position: 50% 50%;
+    margin: -10px;
+    // }
 `
+
 function About () {
     return (
-        <div>
+        <Main>
             <Navbar />
-            <Main>
                 <h1>Rebecca Barrett & Dylan Brisby</h1>
                 <h3>We can't wait to share our special day with you</h3>
-            </Main>
-        </div>
+        </Main>
     )
 }
 
